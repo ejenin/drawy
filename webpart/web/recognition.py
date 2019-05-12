d9a2b505d02306cc9c8fc6f67d0a9d5127f256bd
zukaatoku <zukaatoku@gmail.com>
2019-05-12 20:48:42 +0500

    tweaked learning a bit

--- webpart/web/recognition.py
+++ webpart/web/recognition.py
@@ -28,19 +28,21 @@ classes_file_name = 'classes.txt'
 
 def train():
 
+    script_dir = os.path.abspath(os.path.dirname(__file__))
+    dir = os.path.join(script_dir, "../../" + model_file_name)
 
     # Initialising the CNN
-    exists = os.path.isfile(model_file_name)
+    exists = os.path.isfile(dir)
 
     if exists:
         print('Loading model from file')
-        classifier = load_model(model_file_name)
+        classifier = load_model(dir)
     else:
 
         classifier = Sequential()
 
         # Step 1 - Convolution
-        classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+        classifier.add(Conv2D(32, (3, 3), input_shape = (128, 128, 3), activation = 'relu'))
 
         # Step 2 - Pooling
         classifier.add(MaxPooling2D(pool_size = (2, 2)))
@@ -71,21 +73,21 @@ def train():
         test_datagen = ImageDataGenerator(rescale = 1./255)
 
         training_set = train_datagen.flow_from_directory('digits_dataset/training_set',
-                                                 target_size = (64, 64),
+                                                 target_size = (128, 128),
                                                  batch_size = 32)
 
         test_set = test_datagen.flow_from_directory('digits_dataset/test_set',
-                                            target_size = (64, 64),
+                                            target_size = (128, 128),
                                             batch_size = 32)
 
         classifier.fit_generator(training_set,
                          steps_per_epoch = 1000,
-                         epochs = 1,
+                         epochs = 3,
                          validation_data = test_set,
-                         validation_steps = 200)
+                         validation_steps = 300)
 
         print('Saving model!')
-        classifier.save(model_file_name)
+        classifier.save(dir)
         print('Save success!')
 
 # Part 3 - Making new predictions
@@ -112,7 +114,7 @@ def recognize(imagePath):
     imagePath = os.path.join(script_dir, "../" + imagePath)
     print(imagePath)
     #imagePath = "/webpath/webpart/" + imagePath
-    test_image = image.load_img(imagePath, target_size=(64, 64))
+    test_image = image.load_img(imagePath, target_size=(128, 128))
     test_image = image.img_to_array(test_image)
     test_image = np.expand_dims(test_image, axis=0)
     result = classifier.predict(test_image)
@@ -140,3 +142,6 @@ def get_classes():
         i = i + 1
 
     return lines
+
+
+#train()
